a4e34faf31a1514b5fc6ffa6d9127d6be02e26f4
Guopeng Li <GLI21311@open-groupe.com>
2022-02-01 10:36:52 +0100

    Tracker repository and mappers

---
+++ tracker/tracker_domain/src/main/java/com/example/tracker_domain/repository/TrackerRepository.kt
@@ -0,0 +1,23 @@
+package com.example.tracker_domain.repository
+
+import com.example.tracker_domain.model.TrackableFood
+import com.example.tracker_domain.model.TrackedFood
+import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
+
+// Interface allow to test with fake class
+// Result is a wrapper already provided by google
+interface TrackerRepository {
+
+    suspend fun searchFood(
+        query: String,
+        page: Int,
+        pageSize: Int,
+    ): Result<List<TrackableFood>>
+
+    suspend fun insertTrackedFood(food: TrackedFood)
+
+    suspend fun deleteTrackedFood(food: TrackedFood)
+
+    fun getFoodsForDate(localDate: LocalDate): Flow<List<TrackedFood>>
+}
